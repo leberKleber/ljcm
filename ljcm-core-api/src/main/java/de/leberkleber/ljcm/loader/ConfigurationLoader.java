db26b8025794d3a700198062ca6fdbd078afff80
LeberKleber <max.marche@live.de>
2018-03-07 20:15:13 +0100

    [ljcm-core-api] add javadoc

--- ljcm-core-api/src/main/java/de/leberkleber/ljcm/loader/ConfigurationLoader.java
+++ ljcm-core-api/src/main/java/de/leberkleber/ljcm/loader/ConfigurationLoader.java
@@ -2,6 +2,17 @@ package de.leberkleber.ljcm.loader;
 
 import java.util.Properties;
 
+/**
+ * Implement {@link ConfigurationLoader} to load configurations from custom
+ * source. Minimal set of implementations can be found at {@link de.leberkleber.ljcm.loader.impl}.
+ * See: https://github.com/leberKleber/ljcm/wiki/Configurationloader
+
+ */
 public interface ConfigurationLoader {
+    /**
+     * should return loaded configuration. Return empty {@link Properties} instead
+     * of {@code null}
+     * @return {@link Properties}
+     */
     Properties loadConfigurations();
 }
--- ljcm-core-api/src/main/java/de/leberkleber/ljcm/parser/ConfigurationParser.java
+++ ljcm-core-api/src/main/java/de/leberkleber/ljcm/parser/ConfigurationParser.java
@@ -2,7 +2,27 @@ package de.leberkleber.ljcm.parser;
 
 import java.util.Set;
 
+/**
+ * Implement {@link ConfigurationParser} to parse custom configuration class types.
+ * Minimal set of implementations can be found at {@link de.leberkleber.ljcm.parser.impl}.
+ * See: https://github.com/leberKleber/ljcm/wiki/Configurationparser
+ */
 public interface ConfigurationParser {
+    /**
+     * Should return set of classes for which {@link ConfigurationParser}
+     * is responsible. Primitive Types can also applied to {@link Set<Class>}
+     * e.g.: {@code int.class;}.
+     *
+     * @return {@link Set<Class>}
+     */
     Set<Class> getResponsibleClasses();
+
+    /**
+     * Should parse {@param value} to one of responsible classes, given by
+     * {@link ConfigurationParser#getResponsibleClasses()}
+     *
+     * @param value {@link String}
+     * @return {@link Object instanceof Set<Class>}
+     */
     Object parseValue(String value);
 }
